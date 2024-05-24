da42c74f8b0f92f1cdbdbfb1093d656cb69fb1b8
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-05-24 15:14:37 +0600

    Added artist.schema.ts

---
+++ src/schemas/artist.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Artist {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  description: string;
+}
+
+export type ArtistDocument = Artist & Document;
+export const ArtistSchema = SchemaFactory.createForClass(Artist);
